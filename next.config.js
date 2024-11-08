fe194f7984e492ef99f46598be242db1c157a0de
Zerol Acqua <hlc118@outlook.com>
2024-11-08 22:48:51 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = '/c/acqua-term';
+
 const nextConfig = {
+  basePath: isProd ? basePath : '',
   output: 'export',
+  env: {
+    BASE_PATH: isProd ? basePath : '',
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
+import Link from 'next/link';
+import localFont from 'next/font/local';
+
+// Font files can be colocated inside of `pages`
+const hack = localFont({
+  src: '../assets/fonts/Hack-NF.ttf',
+  fallback: ['monospace'],
+});
 
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
@@ -18,11 +26,11 @@ const App = ({ Component, pageProps }) => {
           key="viewport"
           maximum-scale="1"
         />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href={`${process.env.BASE_PATH}/favicon.ico`} />
       </Head>
 
       <div
-        className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base"
+        className={`${hack.className} text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base`}
         onClick={onClickAnywhere}
       >
         <main className="bg-light-background dark:bg-dark-background w-full h-full p-2">
